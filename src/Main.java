a075b82e7ac0bcb9e0f95718b50000d50fff4bbf
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:24:11 +0200

    Update Main.java
    Remove comments

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,21 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Please input file path: "); //просим пользователя ввести путь к файлу, с которого будем считывать слова
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); //создаем буферизированный
-        // поток для ввода данных с клавиатуры
-        String nameFile = bufferedReader.readLine(); //создаем строковую переменную, в которую будем считывать введенный пользователем
-        // путь к файлу
-        FileReader fileReader = new FileReader(nameFile); //открываем поток для чтения символьных данных из файла
-        BufferedReader reader = new BufferedReader(fileReader); //открываем буферизированный поток, чтобы оптимизировать
-        //работу потока FileReader
+        System.out.println("Please input file path: ");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        
+        String nameFile = bufferedReader.readLine();
+        
+        FileReader fileReader = new FileReader(nameFile);
+        BufferedReader reader = new BufferedReader(fileReader);
 
-        while (reader.ready()) { //цикл будет работать, пока в потоке для чтения будут символьные данные
-            String stringContainer = reader.readLine(); //создаем контейнер для символов, в который будем читать сразу целую
-            //целую строку (строка - набор символов до ввода переноса строки Enter)
+        while (reader.ready()) {
+            String stringContainer = reader.readLine();
 
-            System.out.println(new StringBuilder(stringContainer).reverse().toString()); //выводим в консоль все считанные
-            //символы в зеркальном отображении (для этого используем класс StringBuilder и метод reverse())
+            System.out.println(new StringBuilder(stringContainer).reverse().toString());
         }
-        bufferedReader.close(); //очищаем ресурсы, закрываем поток
-        fileReader.close(); //очищаем ресурсы, закрываем поток
-        reader.close(); //очищаем ресурсы, закрываем поток
+        bufferedReader.close();
+        fileReader.close();
+        reader.close();
     }
 }
